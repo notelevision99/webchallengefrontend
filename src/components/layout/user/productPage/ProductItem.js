662ca95e6bb9e40e604db3053ad8820abdb7f60a
luankat0306 <votriluan99@gmail.com>
2021-03-29 00:51:27 +0700

    add cart

--- src/components/layout/user/productPage/ProductItem.js
+++ src/components/layout/user/productPage/ProductItem.js
@@ -1,30 +1,66 @@
-import React from "react";
+import React from 'react';
 
 function ProductItem({ index, ele }) {
-  return (
-    <div className="product-item" key={index}>
-      <div className="product-item__thumbnail">
-        <img src={ele.photoUrl} alt="" />
+    const addCart = () => {
+        let checkProductExist = 0;
+        let indexProductExist = 0;
+        let product = {
+            id: ele.id,
+            photoUrl: ele.photoUrl,
+            productName: ele.productName,
+            price: ele.price,
+            quantity: 1,
+        };
+        let cart = [];
+        let cartLocal = localStorage.getItem('cart');
+        if (cartLocal) {
+            cart = JSON.parse(cartLocal);
+        }
 
-        <div className="product-item__thumbnail--hover">
-          <p className="hover-title">Chi tiết sản phẩm</p>
+        cart.forEach((element, index) => {
+            if (element.id === product.id) {
+                console.log('index = ' + indexProductExist);
+                checkProductExist = 1;
+                indexProductExist = index;
+                return;
+            }
+        });
 
-          <p className="btn-seen">Xem</p>
+        if (checkProductExist === 1) {
+            cart[indexProductExist].quantity += 1;
+        }
+        if ((checkProductExist === 0 && cart.length === 0) || checkProductExist === 0) {
+            cart.push(product);
+        }
+        localStorage.setItem('cart', JSON.stringify(cart));
+    };
+
+    return (
+        <div className='product-item' key={index}>
+            <div className='product-item__thumbnail'>
+                <img src={ele.photoUrl} alt='' />
+
+                <div className='product-item__thumbnail--hover'>
+                    <p className='hover-title'>Chi tiết sản phẩm</p>
+
+                    <p className='btn-seen'>Xem</p>
+                </div>
+            </div>
+
+            <div className='product-item__info'>
+                <div className='product-item__info--name'>
+                    <h3>{ele.productName}</h3>
+                </div>
+                <div className='product-item__info--price'>
+                    <p>{ele.price}</p>
+
+                    <p className='btn-order' onClick={addCart}>
+                        Đặt hàng
+                    </p>
+                </div>
+            </div>
         </div>
-      </div>
-
-      <div className="product-item__info">
-        <div className="product-item__info--name">
-          <h3>{ele.productName}</h3>
-        </div>
-        <div className="product-item__info--price">
-          <p>{ele.price}</p>
-
-          <p className="btn-order">Đặt hàng</p>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default ProductItem;
