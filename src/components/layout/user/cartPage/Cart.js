7cb7c70f313a60b5593f044972932c8c73f63ae4
luankat0306 <votriluan99@gmail.com>
2021-03-30 13:22:29 +0700

    empty cart

--- src/components/layout/user/cartPage/Cart.js
+++ src/components/layout/user/cartPage/Cart.js
@@ -2,6 +2,7 @@ import { IconDelete } from '../../../../assets/icons';
 import { useHistory } from 'react-router';
 import { useEffect, useState } from 'react';
 import formatVND from '../../../../helpers/user/formatVND';
+import EmptyCart from './EmptyCart';
 
 function Cart() {
     const history = useHistory();
@@ -26,9 +27,8 @@ function Cart() {
         let cartLocal = localStorage.getItem('cart');
         if (cartLocal) {
             setCart(JSON.parse(cartLocal));
+            setTotalPrice(sumPrice);
         }
-
-        setTotalPrice(sumPrice);
     }, [cart]);
 
     const updateFieldChanged = (index) => (e) => {
@@ -43,54 +43,58 @@ function Cart() {
     return (
         <>
             <div className='after-header'></div>
-            <div className='cart-container'>
-                <div className='cart'>
-                    <div className='card'>
-                        <div className='card-header'>
-                            <h3 className='card-title'>Giỏ hàng</h3>
-                        </div>
-                        <div className='card-body'>
-                            {cart.map((element, index) => (
-                                <>
-                                    <div className='cart-item'>
-                                        <img src={element.photoUrl} className='cart-item-left'></img>
-                                        <div className='cart-item-middle'>
-                                            <h3>{element.productName}</h3>
-                                            <br />
-                                            <input
-                                                type='number'
-                                                min='1'
-                                                defaultValue='1'
-                                                value={element.quantity}
-                                                onChange={updateFieldChanged(index)}></input>
+            {cart !== null ? (
+                <div className='cart-container'>
+                    <div className='cart'>
+                        <div className='card'>
+                            <div className='card-header'>
+                                <h3 className='card-title'>Giỏ hàng</h3>
+                            </div>
+                            <div className='card-body'>
+                                {cart.map((element, index) => (
+                                    <>
+                                        <div className='cart-item'>
+                                            <img src={element.photoUrl} className='cart-item-left'></img>
+                                            <div className='cart-item-middle'>
+                                                <h3>{element.productName}</h3>
+                                                <br />
+                                                <input
+                                                    type='number'
+                                                    min='1'
+                                                    defaultValue='1'
+                                                    value={element.quantity}
+                                                    onChange={updateFieldChanged(index)}></input>
+                                            </div>
+                                            <h4 className='cart-item-right'>{formatVND(element.price)}</h4>
+                                            <div className='icon-d'>
+                                                <IconDelete />
+                                            </div>
                                         </div>
-                                        <h4 className='cart-item-right'>{formatVND(element.price)}</h4>
-                                        <div className='icon-d'>
-                                            <IconDelete />
-                                        </div>
-                                    </div>
-                                    <hr />
-                                </>
-                            ))}
+                                        <hr />
+                                    </>
+                                ))}
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div className='cart-total'>
-                    <div className='card'>
-                        <div className='card-header'>
-                            <h3 className='card-title'>Thông tin thanh toán</h3>
-                        </div>
-                        <div className='card-body'>
-                            <h3>Tổng tiền</h3>
-                            <h4>{formatVND(totalPrice)}</h4>
-                        </div>
-                        <div className='card-footer'>
-                            <button onClick={onClickNextCheckout}>Đặt hàng</button>
+                    <div className='cart-total'>
+                        <div className='card'>
+                            <div className='card-header'>
+                                <h3 className='card-title'>Thông tin thanh toán</h3>
+                            </div>
+                            <div className='card-body'>
+                                <h3>Tổng tiền</h3>
+                                <h4>{formatVND(totalPrice)}</h4>
+                            </div>
+                            <div className='card-footer'>
+                                <button onClick={onClickNextCheckout}>Đặt hàng</button>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            ) : (
+                <EmptyCart />
+            )}
         </>
     );
 }
